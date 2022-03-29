d5fc6551105849ef11b0193b1037c0f2e98f762b
Valen <valenpschool@gmail.com>
2022-03-29 18:09:36 -0300

    commit

---
+++ asp-filmes/Models/cl_filmes.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+
+namespace asp_filmes.Models
+{
+    public class cl_filmes
+    { public string cod_gen { get; set; }
+        [DisplayName("Gênero do filme")]
+        public string gen { get; set; }
+
+      public string cod_dir { get; set; }
+        [DisplayName("Diretor do filme")]
+        public string dir { get; set; }
+
+      public string cod_filme { get; set; }
+        [DisplayName("Nome do filme")]
+        public string filme { get; set; }
+    }
+}
